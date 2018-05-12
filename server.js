f477d5ee571a6988a624525b86da94dad1b754f5
Indrajeet Mishra <indrajeet.mishra@ericsson.com>
2018-05-12 14:54:16 +0530

    home page removed

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((req,res,next)=>{
 
 app.get('/',(req,res)=>{
     
-    res.render('about.hbs',
+    res.render('home.hbs',
                {
                    title:'home page'
                    ,header:'welcome to the home page'
